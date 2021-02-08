64e1fd38c31390522f2e81d3280d60e32b20b544
Taimoor Khan <46717905+Taimoorkhan1122@users.noreply.github.com>
2021-02-08 14:26:31 +0500

    any and unkown | learning union and intersection

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,20 @@
 "use strict";
-console.log("hello world");
-console.log("hello 2");
+var skills;
+(function (skills) {
+    skills["FRONT_END"] = "React and other front end technologies";
+    skills["BACK_END"] = "Node and other related technologies";
+})(skills || (skills = {}));
+let data1 = {
+    name: "Taimoor khan",
+    age: 21,
+    status: true,
+    skills: skills.FRONT_END,
+    20: "hello world",
+};
+let a = {
+    b: "x",
+    c: "hello",
+};
+const e = { name: "Mansoor", NIC: 4240 | 445 };
+console.log(e);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,34 @@
-console.log("hello world");
-console.log("hello 2");
+enum skills {
+  FRONT_END = "React and other front end technologies",
+  BACK_END = "Node and other related technologies",
+}
+
+type data = {
+  name: string;
+  age: number;
+  status: boolean;
+  skills: skills;
+  [key: number]: string;
+};
+
+let data1: data = {
+  name: "Taimoor khan",
+  age: 21,
+  status: true,
+  skills: skills.FRONT_END,
+  20: "hello world",
+};
+
+let a: object = {
+  b: "x",
+  c: "hello",
+};
+
+type employee = {
+  name: string;
+  NIC: string | number; // NIC can have  have a type either both string or number only
+};
+
+const e: employee = { name: "Mansoor", NIC: 4240 };
+
+console.log(e);
